ebb2156d48ec48a5d81a35d36816e506e1f0f4f9
william-redenbaugh <wmredenbaugh@gmail.com>
2020-11-05 23:37:58 -0800

    removing stuff that shouldn't be there

--- Will-OSSTM32F4 Port/src/main.cpp
+++ Will-OSSTM32F4 Port/src/main.cpp
@@ -32,10 +32,9 @@ void setup() {
   os_init(); 
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT); 
-  os_add_thread(led_blink_thread, NULL, sizeof(thread_stack), thread_stack);  
+ // os_add_thread(led_blink_thread, NULL, sizeof(thread_stack), thread_stack);  
   os_add_thread(led_blink_thread_two, NULL, sizeof(thread_stack_two), thread_stack_two);  
 
-  a_function(); 
 
   // With the stm32, we don't currently use the primary thread. 
   _os_yield(); 
