570e0a6ac615ce98002a8b4b6fbe485d761850d8
William Redenbaugh <wmredenbaugh@gmail.com>
2020-11-10 01:01:45 -0800

    updated everything to use the master branch since I finished merging all my changes

--- Teensy4Port/src/main.cpp
+++ Teensy4Port/src/main.cpp
@@ -1,14 +1,29 @@
 #include <Arduino.h>
 #include "OS/OSThreadKernel.h"
 
+void led_thread(void *parameters){
+	for(;;){
+		os_thread_sleep_ms(1000);
+		digitalWrite(LED_BUILTIN, HIGH);
+		os_thread_sleep_ms(1000);
+		digitalWrite(LED_BUILTIN, LOW);
+	}
+}
+
+void another_led_thread(void *parameters){
+	for(;;){
+		
+	}
+}
+
 void setup() {
+	delay(2000);
 	os_init(); 
 	pinMode(LED_BUILTIN, OUTPUT);
+	os_add_thread(&another_led_thread, NULL, -1, NULL);
+	os_add_thread(&led_thread, NULL, -1, NULL); 
+	_os_yield();
 }
 
 void loop() {
-	os_thread_delay_ms(1000);
-	digitalWrite(LED_BUILTIN, HIGH);
-	os_thread_delay_ms(1000);
-	digitalWrite(LED_BUILTIN, LOW);
 }
